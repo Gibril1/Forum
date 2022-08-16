562fc9300095799c097f410bf621a1865be27229
Jibril <gibril@yahoo.com>
2022-08-16 04:04:27 -0700

    added some styles

--- forum/__init__.py
+++ forum/__init__.py
@@ -3,12 +3,18 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 
-
+# Init app
 app = Flask(__name__)
+
 app.config['SECRET_KEY'] = 'f60fd60910668fd09fe5'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+# Init db
 db = SQLAlchemy(app)
+# Init bcrypt
 bcrypt = Bcrypt(app)
+# Init login manager
 login_manager = LoginManager(app)
 login_manager.login_view = 'login'
 
